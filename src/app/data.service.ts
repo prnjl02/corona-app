a3c5f05130e925eee5b870803908f57b80bc65e5
angularinfotrench <angular.infotrench@gmail.com>
2021-10-11 22:22:04 +0530

    api endpoint change by open source

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,12 +1,11 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-@Injectable({providedIn:'root'})
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+@Injectable({ providedIn: "root" })
 export class DataService {
-    constructor(private http:HttpClient){}
+  name = "pranjal";
+  constructor(private http: HttpClient) {}
 
-    getDailyData(){
-        return this.http.get(
-            'https://api.covid19india.org/data.json'
-        );
-    }
+  getDailyData() {
+    return this.http.get("https://data.covid19india.org/data.json");
+  }
 }
